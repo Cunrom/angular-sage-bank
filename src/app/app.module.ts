502a2d0e2d503ae529af90857af0ebaf8dc11dd3
Cunrom <johntruong06@gmail.com>
2021-08-09 21:21:11 -0400

    Heroku Compatibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http'
 
@@ -23,7 +23,6 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
   imports: [
     BrowserModule,
     RouterLinkModule,
-    FormsModule,
     HttpClientModule,
     ReactiveFormsModule
   ],
